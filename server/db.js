2de53a4c6f7e5e6cc6448c30e7f55b7dcecceff3
AndrewSushi <andrewsushi8@gmail.com>
2024-10-20 22:34:20 -0700

    Created the db pool

---
+++ server/db.js
@@ -0,0 +1,26 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+const checkDatabaseConnection = async () => {
+    try {
+        await pool.query('SELECT NOW()');
+        console.log('Database connected successfully');
+    } catch (err) {
+        console.error('Database connection failed', err);
+        process.exit(1);
+    }
+};
+
+export { pool, checkDatabaseConnection };
