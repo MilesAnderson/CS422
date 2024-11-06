c37605da35ffc0f5a209debc503b7d03359f7836
frankwoods <liamtbo@gmail.com>
2024-11-05 18:56:48 -0800

    andrew says to pull

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import { checkDatabaseConnection } from './db.js'
 
 import stocksApi from "./routes/stocksApi.js";
 import userRoutes from './routes/userRoutes.js';
-import test from "./routes/test.js";
+// import test from "./routes/test.js";
 import watchlistRoutes from "./routes/watchlistRoutes.js";
 
 const app = express(); // Express application created
@@ -33,7 +33,7 @@ when user does
 app.use('/api/stocks', stocksApi), server will invoke functions defined
 at stocksApi.js which was defined above as import stocksApi from "./routes/stocksApi.js";
 */
-app.use('/test', test);
+// app.use('/test', test);
 app.use('/api/stocks', stocksApi);
 app.use('/api', userRoutes);
 app.use('/api/watchlist', watchlistRoutes);
